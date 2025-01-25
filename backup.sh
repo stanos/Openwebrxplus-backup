cf32b9553f8d611d1c7783a7c8c21086b7d27280
Stanos <stan@sailingstanos.nl>
2025-01-25 15:35:12 +0000

    $(date +%F)

--- backup.sh
+++ backup.sh
@@ -2,7 +2,10 @@
 
 cp -r /var/lib/openwebrx/ /home/stanos/Openwebrxplus-backup/vlopenwebrx/
 cp -r /etc/openwebrx/ /home/stanos/Openwebrxplus-backup/etcopenwebrx/
-cd /home/satnos/Openwebrxplus-backup/
-git add .
-git commit -m "$(date +%F)"
-git push -u origin main
+pwd
+chown stanos:stanos * -R
+chmod -R ugo+rwX .
+su - stanos -c "git add vlopenwebrx"
+su - stanos -c "git add etcopenwebrx"
+su - stanos -c "git commit -m '$(date +%F)' "
+su - stanos -c "git push -u origin main" 
